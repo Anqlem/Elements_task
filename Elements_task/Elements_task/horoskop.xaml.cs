f19dadb6f0d81dc76cc5d473b83b58ada76db25f
Opilane TTHK <opilane@tthk.ee>
2020-11-11 11:15:53 +0200

    Add project files.

---
+++ Elements_task/Elements_task/horoskop.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+
+namespace Elements_task
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class horoskoop : ContentPage
+    {
+        public horoskoop()
+        {
+            InitializeComponent();
+        }
+
+        private void changehoros(object sender, DateChangedEventArgs e)
+        {
+            change.Text = e.NewDate.ToLongDateString();
+            if (change.Text == "Friday, November 13, 2020")
+            {
+                horos.Source = "Aries.png";
+            }
+            else if(change.Text== "Friday, November 12, 2020")
+            {
+                horos.Source = "Cancer.png";
+            }
+        }
+
+    }
+}
